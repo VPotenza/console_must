0b4ff9b88760a21ef5fa0b0a84a6211ec2edde2e
VPotenza <94545933+VPotenza@users.noreply.github.com>
2021-12-22 14:09:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_must
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("İSMİNİZ NEDİR?");
+            string ad = Console.ReadLine();
+            Console.WriteLine("SOYİSMİNİZ NEDİR?");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("BABA ADI NEDİR?");
+            string baba = Console.ReadLine();
+            Console.WriteLine("Selamlar " + baba + " evladı " + ad + " "+ soyad );
+        }
+    }
+}
